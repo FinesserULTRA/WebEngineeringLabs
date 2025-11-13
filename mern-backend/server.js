3be310a34a000e493cd38aee258317107a53f1d1
FinesserULTRA <mustafahamad555@gmail.com>
2025-11-13 11:39:19 +0500

    complete MERN

--- mern-backend/server.js
+++ mern-backend/server.js
@@ -8,16 +8,18 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/mernlab', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/mernlab')
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
 // Test Route
 app.get('/', (req, res) => {
   res.send('Backend is running!');
+});
+
+// Users API Route
+app.get('/api/users', (req, res) => {
+  res.json({ message: 'Get all users', users: [] });
 });
 
 const PORT = process.env.PORT || 5000;
